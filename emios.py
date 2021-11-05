4da5e95a2bc66e21a71a60db50e121e703bf8cd4
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2021-11-05 09:22:29 +0600

    Update emios.py

--- emios.py
+++ emios.py
@@ -1,138 +1,48 @@
-import os
-import time
-import json
 import discord
-import requests
 from googletrans import Translator
 
-client = discord.Client()
+emios = discord.Client()
 
 class Emios:
-    author_id = 723074321383424040
-    translator = Translator()
-    will_translate = False
-    channel_lang = []
-    ping = False
+	translator = Translator()
 
-    
-    @staticmethod
-    def channel_translation_setup(message):
-        if message.content.startswith('--start') and message.author.id == Emios.author_id:        
-            command_args = message.content.split(" ")
-            Emios.channel_lang.append([message.channel, command_args[1].split("=")[1]])
+	@staticmethod
+	def reverse_message(message):
+		return message.channel.send(f"Reversed Message: {message.content[::-1]}")
 
-        elif message.content.startswith('--stop') and message.author.id == Emios.author_id:
-            for index, active in enumerate(Emios.channel_lang):
-                if active[0] == message.channel:
-                    Emios.channel_lang.pop(index)
+	@staticmethod
+	def translate(message):
+		args = message.content.split("! ")[0]
+		msg = message.content.split("! ")[1]
 
-        return None
-    
-    @staticmethod
-    def translate(message):
-        lang = "auto"
-        if not message.author.bot and not message.author.id == Emios.author_id:
-            for index, active in enumerate(Emios.channel_lang):
-                if active[0] == message.channel:
-                    lang = active[1]
-                    translation = Emios.translator.translate(str(message.content), src=str(lang), dest="en")
-                    corrected_word = translation.extra_data["possible-mistakes"]
-                    if corrected_word:
-                        return f"[{message.author}] {Emios.translator.translate(text=corrected_word[1], src=lang, dest='en').text}"
+		try:
+			arg_from = args.find("from=")
+			arg_to = args.find("to=")
 
-                    return f"[{message.author}] {translation.text}"
-        
-        return ""
-
-    @staticmethod
-    def force_translate(message):
-        if True: #message.author.id == Emios.author_id:
-            if message.content.startswith("--translate"):
-               command_args = message.content.split(" ")
-               lang = str(command_args[1].split("=")[1])
-               text = str(message.content[message.content.index("=")+3:])
-               translation = Emios.translator.translate(text, src=lang, dest="en")
-               corrected_word = translation.extra_data["possible-mistakes"]
-
-               if corrected_word:
-                   return Emios.translator.translate(text=corrected_word[1], src=lang, dest="en").text
-
-               return translation.text
-
-        return ""
+			src = args[arg_from+len("from="):arg_from+len("from=")+2]
+			dest = args[arg_to+len("to="):arg_to+len("to=")+2]
 
 
-    @staticmethod
-    def force_translate__translation_object(message):
-        if True: #message.author.id == Emios.author_id:
-            if message.content.startswith("--translate-return-object"):
-                command_args = message.content.split(" ")
-                lang = str(command_args[1].split("=")[1])
-                text = str(message.content[message.content.index("=")+3:])
-                translation = Emios.translator.translate(text, src=lang, dest="en")
-                return (translation, translation.extra_data)
+			translation = Emios.translator.translate(msg, src=src, dest=dest)
+		
+		except Exception as error:
+			translation = Emios.translator.translate(msg)
 
-        return ""
-
-    @staticmethod
-    def ping_user(message):
-        if True: #message.author.id == Emios.author_id:
-            if message.content.startswith("--ping-user"):
-                command_args = message.content.split(" ")
-                user = command_args[1]
-
-                return f"{user}"
-        
-        return ""
-
-    @staticmethod
-    def stop_ping(message):
-        if True: #message.author.id == Emios.author_id:
-            if message.content.startswith("--stop-ping"):
-                Emios.ping = False
-
-                return True
-
-        return ""
-    
-    @staticmethod
-    def get_quote():
-        response = requests.get("https://zenquotes.io/api/random")
-        json_data = json.loads(response.text)
-        quote = json_data[0]['q'] + " -" + json_data[0]['a']
-        
-        return(quote)
+		return message.channel.send(f"Translation: {translation.text}")
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
+@emios.event
 async def on_message(message):
-    if message.content.startswith('$inspire'):
-        quote = Emios.get_quote()
-        await message.channel.send(quote)
+	if message.author == emios.user:
+		return
 
-    Emios.channel_translation_setup(message)
-    if Emios.translate(message):
-        await message.channel.send(Emios.translate(message))
+	if message.content.startswith("r! "):
+		await Emios.reverse_message(message)
 
-    if Emios.force_translate(message):
-        await message.channel.send(Emios.force_translate(message))
+	elif message.content.startswith("translate ") or message.content.startswith("translate!"):
+		await Emios.translate(message)
 
-    if Emios.force_translate__translation_object(message):
-        await message.channel.send(Emios.force_translate__translation_object(message))
-    
-    if Emios.ping_user(message):
-        Emios.ping = True
-        while Emios.ping:
-            await message.channel.send(Emios.ping_user(message))
-            time.sleep(1)
-
-    if Emios.stop_ping(message):
-        await message.channel.send("stopped")
-
+        
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
 
 
