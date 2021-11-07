3fe437cc69b64cd2b48688835dc0dec4450eda28
Araf <67403822+Sadman-Hasin@users.noreply.github.com>
2021-11-07 10:33:07 +0600

    Update emios.py

--- emios.py
+++ emios.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from googletrans import Translator
+from hadith import Hadith
 
 emios = discord.Client()
 
@@ -30,8 +31,22 @@ class Emios:
 			translation = Emios.translator.translate(msg)
 
 		return message.channel.send(f"Translation: {translation.text}")
+	
+	@staticmethod
+	def getHadith(message):
+		hadith = Hadith.fetch_random()
+		collection = hadith[2]["collection"]
 
+		chapterNumber = hadith[2]["hadith"][0]["chapterNumber"]
+		chapterTitle = hadith[2]["hadith"][0]["chapterTitle"]
+		raw_hadith_body = hadith[2]["hadith"][0]["body"]
+		hadith_body = raw_hadith_body[3:raw_hadith_body.find("<br/><br/><b>")]
+		hadith_book = raw_hadith_body[raw_hadith_body.find("<br/><br/><b>")+len("<br/><br/><b>"):raw_hadith_body.find("</b>.<br/><br/></p>")]
+		body = f"{hadith_body}\n{hadith_book}"
 
+		return message.channel.send(f"Collection: {collection.capitalize()}\n\nChapter title: {chapterTitle}\nChapter number: {chapterNumber}\n\n{body}")
+
+	
 @emios.event
 async def on_message(message):
 	if message.author == emios.user:
@@ -42,7 +57,9 @@ async def on_message(message):
 
 	elif message.content.startswith("translate ") or message.content.startswith("translate!"):
 		await Emios.translate(message)
-
+	
+	elif message.content.startswith("hadith!"):
+		await Emios.getHadith(message)
         
 emios.run(os.getenv("DISCORD_BOT_TOKEN"))
 
